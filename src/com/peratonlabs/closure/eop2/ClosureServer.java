d27cd79bfb2670f4ac123af676feed0baef0b547
Ta Chen <tchen@peratonlabs.com>
2022-06-26 09:00:14 -0400

    reorg server

--- src/com/peratonlabs/closure/eop2/ClosureServer.java
+++ src/com/peratonlabs/closure/eop2/ClosureServer.java
@@ -13,6 +13,7 @@ import io.undertow.Handlers;
 import io.undertow.Undertow;
 import io.undertow.server.HttpHandler;
 import io.undertow.server.RoutingHandler;
+import io.undertow.server.handlers.PathHandler;
 import io.undertow.server.handlers.resource.ClassPathResourceManager;
 import static io.undertow.Handlers.path;
 import static io.undertow.Handlers.resource;
@@ -32,46 +33,44 @@ public class ClosureServer
             .get("/new*", RoutingHandlers.plainTextHandler("GET - new*"))
             .setFallbackHandler(RoutingHandlers::notFoundHandler);
     
-    private void serve() {
-        Undertow server = Undertow.builder()
-                .addHttpListener(8080, "localhost")
-                .setHandler(
-                    path()
-                        .addPrefixPath("/video", VideoManager.createWebSocketHandler())
-                        .addPrefixPath("/", resource(new ClassPathResourceManager(ClosureServer.class.getClassLoader()))
-                                                    .addWelcomeFiles("index.html"))
-                        
-                        .addPrefixPath("/about", RoutingHandlers.plainTextHandler("GET - about"))
-                        .addPrefixPath("/controller", Handlers.routing()
-                                .post("/{id}", exchange -> {
-                                    String id = exchange.getQueryParameters().get("id").getFirst();
-                                    System.out.println("############ " + id);
-                                })
-                                )
-                        .addPrefixPath("/request", Handlers.routing()
-                                .post("/{request}", VideoRequester.createRequest())
-                                )
-                        // REST API path
-                        .addPrefixPath("/api", Handlers.routing()
-                            .get("/customers", exchange -> {System.out.println("customers");})
-                            .delete("/customers/{customerId}", exchange -> {System.out.println("delete");})
-                            .setFallbackHandler(exchange -> {System.out.println("fallback");}))
+    private HttpHandler handler = new PathHandler()
+            .addPrefixPath("/video", VideoManager.createWebSocketHandler())
+            .addPrefixPath("/", resource(new ClassPathResourceManager(ClosureServer.class.getClassLoader()))
+                                        .addWelcomeFiles("index.html"))
+            
+            .addPrefixPath("/about", RoutingHandlers.plainTextHandler("GET - about"))
+            .addPrefixPath("/controller", Handlers.routing()
+                    .post("/{id}", exchange -> {
+                        String id = exchange.getQueryParameters().get("id").getFirst();
+                        System.out.println("############ " + id);
+                    })
+                    )
+            .addPrefixPath("/request", Handlers.routing()
+                    .post("/{request}", VideoRequester.createRequest())
+                    )
+            // REST API path
+            .addPrefixPath("/api", Handlers.routing()
+                .get("/customers", exchange -> {System.out.println("customers");})
+                .delete("/customers/{customerId}", exchange -> {System.out.println("delete");})
+                .setFallbackHandler(exchange -> {System.out.println("fallback");}))
 
-                        // Redirect root path to /static to serve the index.html by default
-//                        .addExactPath("/", Handlers.redirect("/static"))
+            // Redirect root path to /static to serve the index.html by default
+//            .addExactPath("/", Handlers.redirect("/static"))
 
-                        // Serve all static files from a folder
-//                        .addPrefixPath("/static", new ResourceHandler(
-//                            new PathResourceManager(Paths.get("/path/to/www/"), 100))
-//                            .setWelcomeFiles("index.html"))                        
-                )
-                .build();
-        server.start();
-    }
+            // Serve all static files from a folder
+//            .addPrefixPath("/static", new ResourceHandler(
+//                new PathResourceManager(Paths.get("/path/to/www/"), 100))
+//                .setWelcomeFiles("index.html"))                        
+    ;
     
     public static void main(final String[] args) {
-        ClosureServer server = new ClosureServer();
-        server.serve();
+        ClosureServer closure = new ClosureServer();
+        
+        Undertow server = Undertow.builder()
+                .addHttpListener(8080, "localhost")
+                .setHandler(closure.handler)
+                .build();
+        server.start();
     }
 
 }
