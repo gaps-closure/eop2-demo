4d1cad0e2989c7c17f674cb4639441a308e00cb4
Ta Chen <tchen@peratonlabs.com>
2022-07-06 15:52:05 -0400

    normal

--- src/com/peratonlabs/closure/eop2/level/normal/VideoServerNormal.java
+++ src/com/peratonlabs/closure/eop2/level/normal/VideoServerNormal.java
@@ -1,57 +1,15 @@
 package com.peratonlabs.closure.eop2.level.normal;
 
-public class VideoServerNormal implements Runnable
+import com.peratonlabs.closure.eop2.level.VideoServer;
+
+public class VideoServerNormal extends VideoServer implements Runnable
 {
     private static boolean started = false;
     
-    protected String webroot;
-    protected int port = 8080;
-    
-    private VideoServerNormal(int port, String webroot) {
+    private VideoServerNormal(int port, String webroot, VideoServletNormal servlet) {
         this.port = port;
         this.webroot = webroot;
-    }
-    
-    public void serve() {
-        @SuppressWarnings("serial")
-        class MyServ extends Acme.Serve.Serve {
-            // Overriding method for public access
-            public void setMappingTable(PathTreeDictionary mappingtable) { 
-                super.setMappingTable(mappingtable);
-            }
-            // add the method below when .war deployment is needed
-            public void addWarDeployer(String deployerFactory, String throttles) {
-                super.addWarDeployer(deployerFactory, throttles);
-            }
-            public void addWebsocketProvider() { // add if plan to deploy websocket endpoints
-                addWebsocketProvider(null); // list of class path file components can be provided here
-            }
-
-        };
-
-        final MyServ srv = new MyServ();
-        // setting aliases, for an optional file servlet
-        Acme.Serve.Serve.PathTreeDictionary aliases = new Acme.Serve.Serve.PathTreeDictionary();
-        aliases.put("/*", new java.io.File(webroot));
-        //  note cast name will depend on the class name, since it is anonymous class
-        srv.setMappingTable(aliases);
-        // setting properties for the server, and exchangeable Acceptors
-        java.util.Properties properties = new java.util.Properties();
-        properties.put("port", port);
-        properties.setProperty(Acme.Serve.Serve.ARG_NOHUP, "nohup");
-        properties.setProperty("acceptorImpl", "Acme.Serve.SelectorAcceptor"); // this acceptor is requireed for websocket support
-        srv.arguments = properties;
-        srv.addDefaultServlets(null); // optional file servlet
-        srv.addWebsocketProvider();  // enable websocket
-        srv.addServlet("/request", new VideoServletNormal()); // optional
-        // the pattern above is exact match, use /myservlet/* for mapping any path startting with /myservlet (Since 1.93)
-        Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
-            public void run() {
-                srv.notifyStop();
-                srv.destroyAllServlets();
-            }
-        }));
-        srv.serve();
+        this.servlet = servlet;
     }
     
     public static void startServer(int port, String webroot) {
@@ -59,15 +17,11 @@ public class VideoServerNormal implements Runnable
             return;
         }
         
-        VideoServerNormal instance = new VideoServerNormal(port, webroot);
+        VideoServletNormal servlet = new VideoServletNormal();
+        VideoServerNormal instance = new VideoServerNormal(port, webroot, servlet);
         Thread thread = new Thread(instance);
         thread.start();
         
         started = true;
-    }
-
-    @Override
-    public void run() {
-        serve();
     }
 }
--- src/com/peratonlabs/closure/eop2/level/normal/VideoServletNormal.java
+++ src/com/peratonlabs/closure/eop2/level/normal/VideoServletNormal.java
@@ -4,9 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 
 import java.io.PrintWriter;
-import java.util.Date;
-import java.util.Set;
-
 import javax.servlet.annotation.WebServlet;
 
 import javax.servlet.http.HttpServlet;
@@ -15,30 +12,16 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.peratonlabs.closure.eop2.video.requester.Request;
 
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletContainerInitializer;
-import javax.servlet.ServletContext;
-import javax.servlet.ServletContextEvent;
-import javax.servlet.ServletContextListener;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import javax.servlet.annotation.WebListener;
 import javax.servlet.annotation.ServletSecurity;
-import javax.servlet.annotation.WebFilter;
 import javax.servlet.annotation.WebInitParam;
-import javax.servlet.annotation.HandlesTypes;
 
 @SuppressWarnings("serial")
 @WebServlet(
     name = "AnnotatedServlet", 
-    description = "A annotated servlet", 
+    description = "An annotated servlet", 
     urlPatterns = { "/request" }, 
     initParams = { 
-        @WebInitParam(name = "foo", value = "Hello "),
-        @WebInitParam(name = "bar", value = " World!") 
+        @WebInitParam(name = "GAPS", value = " CLOSURE!") 
     }
 )
 @ServletSecurity()
@@ -46,7 +29,7 @@ public class VideoServletNormal extends HttpServlet
 {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         PrintWriter writer = response.getWriter();
-        writer.println("<html>OK!</html>");
+        writer.println("<html>Got it!</html>");
         writer.flush();
     }
 
@@ -62,76 +45,13 @@ public class VideoServletNormal extends HttpServlet
             e.printStackTrace();
         }
 
-        System.out.println(jb.toString());
+        System.out.println("Normal: " + jb.toString());
         Request req = Request.fromJson(jb.toString());
+        
         VideoRequesterNormal.handleRequest(req);
         
         PrintWriter writer = response.getWriter();
         writer.println("<html>OK</html>");
         writer.flush();
-    }
-
-    @WebFilter(
-        urlPatterns = { "/*" }, 
-        initParams = {
-            @WebInitParam(
-                name = "test-param", 
-                value = "Initialization Paramter")
-        }
-    )
-    
-    public static class LogFilter implements Filter {
-        @Override
-        public void init(FilterConfig config) throws ServletException {
-            // Get init parameter
-            String testParam = config.getInitParameter("test-param");
-
-            // Print the init parameter
-            System.out.println("Test Param: " + testParam);
-        }
-
-        @Override
-        public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-                throws IOException, ServletException {
-            // Log the current timestamp.
-            System.out.println("Time " + new Date().toString());
-
-            // Pass request back down the filter chain
-            chain.doFilter(request, response);
-        }
-
-        @Override
-        public void destroy() {
-            /*
-             * Called before the Filter instance is removed from service by the web
-             * container
-             */
-            System.out.println("Filter destroied");
-        }
-    }
-
-    @WebListener
-    public static class ContextListener implements ServletContextListener {
-        @Override
-        public void contextInitialized(ServletContextEvent event) {
-            System.out.println("The application started");
-        }
-
-        @Override
-        public void contextDestroyed(ServletContextEvent event) {
-            System.out.println("The application stopped");
-        }
-    }
-
-    @HandlesTypes({
-        javax.servlet.http.HttpServlet.class,
-        javax.servlet.Filter.class
-    })
-    public static class AppInitializer implements ServletContainerInitializer {
-        @Override
-        public void onStartup(Set<Class<?>> classes, ServletContext context)
-                throws ServletException {
-            System.out.println("Classes "+classes+" getting initialized.");
-        }
     }
 }
